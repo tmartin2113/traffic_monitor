760324cbabd2ad9a0fa70677d823e36c933e8054
tmartin2113 <tmartin2113@gmail.com>
2025-09-22 11:59:52 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,40 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@stores': path.resolve(__dirname, './src/stores'),
+      '@types': path.resolve(__dirname, './src/types'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+    cors: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'leaflet-vendor': ['leaflet', 'react-leaflet'],
+          'utils-vendor': ['axios', 'date-fns', 'clsx'],
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ['leaflet', 'react-leaflet'],
+  },
+});
