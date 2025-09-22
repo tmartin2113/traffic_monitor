e27be72fdb7aea0654b8b9b3367d54b4c83e44b3
tmartin2113 <tmartin2113@gmail.com>
2025-09-22 12:04:51 -0500

    Update .eslintre.cjs

--- .eslintre.cjs
+++ .eslintre.cjs
@@ -1,1 +1,124 @@
-
+module.exports = {
+  root: true,
+  env: { 
+    browser: true, 
+    es2020: true,
+    node: true 
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+  ],
+  ignorePatterns: [
+    'dist',
+    '.eslintrc.cjs',
+    'vite.config.ts',
+    'postcss.config.js',
+    'tailwind.config.js',
+    '*.html',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: [
+    'react-refresh',
+    '@typescript-eslint',
+    'react'
+  ],
+  rules: {
+    // React Rules
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/prop-types': 'off',
+    'react/display-name': 'off',
+    
+    // TypeScript Rules
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { 
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      }
+    ],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/await-thenable': 'error',
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      {
+        checksVoidReturn: {
+          attributes: false,
+        },
+      },
+    ],
+    
+    // General Rules
+    'no-console': [
+      'warn',
+      { 
+        allow: ['warn', 'error'] 
+      }
+    ],
+    'no-debugger': 'warn',
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'object-shorthand': 'error',
+    'prefer-template': 'error',
+    'prefer-destructuring': [
+      'error',
+      {
+        array: false,
+        object: true,
+      },
+    ],
+    
+    // Code Quality
+    'eqeqeq': ['error', 'always'],
+    'curly': ['error', 'all'],
+    'no-throw-literal': 'error',
+    'no-return-await': 'error',
+    'require-await': 'error',
+    'no-duplicate-imports': 'error',
+    'no-useless-constructor': 'error',
+    'no-empty-function': [
+      'error',
+      {
+        allow: ['arrowFunctions', 'constructors'],
+      },
+    ],
+    
+    // Formatting (if not using Prettier)
+    'indent': ['error', 2, { 'SwitchCase': 1 }],
+    'quotes': ['error', 'single', { 'avoidEscape': true }],
+    'semi': ['error', 'always'],
+    'comma-dangle': ['error', 'only-multiline'],
+    'arrow-parens': ['error', 'as-needed'],
+    'max-len': [
+      'warn',
+      {
+        code: 100,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+      },
+    ],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
